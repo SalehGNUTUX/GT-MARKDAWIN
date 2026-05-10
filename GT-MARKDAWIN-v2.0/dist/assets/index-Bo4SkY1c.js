e71067298ce63cb4e120033dbd9cfeb75a83c23c
GNUTUX <salehoukiki@gmail.com>
2026-05-10 15:27:13 +0100

    Add files via upload

---
+++ GT-MARKDAWIN-v2.0/dist/assets/index-Bo4SkY1c.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D4uKroqh.js","./index-BjSsOXs8.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-Cp59_y6s.js";import{r as p}from"./index-BjSsOXs8.js";const i=p("App",{web:()=>e(()=>import("./web-D4uKroqh.js"),__vite__mapDeps([0,1]),import.meta.url).then(r=>new r.AppWeb)});export{i as App};
